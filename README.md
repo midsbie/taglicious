ec095dbc45abf350c5711894788dce5bcda31dc9
Miguel Guedes <miguel@softgeist.com>
2024-05-15 18:16:22 +0100

    Genesis

---
+++ README.md
@@ -0,0 +1,76 @@
+# Taglicious
+
+Taglicious is a versatile and extensible React component for managing tags and multi-select inputs
+with customizable rendering and seamless integration with various UI libraries.
+
+## Features
+
+- Customizable rendering of input fields and tags.
+- Context-based rendering for seamless integration with different UI libraries.
+- Event handling for adding, filtering, and removing tags.
+- Clear button for resetting the input and tags.
+- Modular architecture allowing framework-specific implementations.
+    
+## Getting Started
+
+This guide will help you set up and use the Taglicious library in your project.
+
+### Installation
+
+Start by installing the dependencies with Yarn:
+
+```sh
+yarn install
+```
+
+### Usage
+
+Import the required components and types from the library:
+
+```jsx
+import { Taglicious } from '@taglicious/react-bootstrap';
+import { Tag } from '@taglicious/model';
+```
+
+Define your tags and input handlers:
+
+```jsx
+const tags: Tag[] = [
+  { value: 'tag1', label: 'Tag 1' },
+  { value: 'tag2', label: 'Tag 2' },
+  // ... more tags
+];
+
+const handleInputChange = React.useCallback((input: string, action: "filter" | "add"): boolean => {
+  // Handle input change for filtering or adding tags
+  return true;
+}, []);
+
+const handleRemove = React.useCallback((tag: Tag) => {
+  // Handle tag removal
+}, []);
+
+const handleClear = React.useCallback(() => {
+  // Handle clearing all tags
+}, []);
+```
+
+Render the `Taglicious` component with your tags and handlers:
+
+```jsx
+<Taglicious
+  placeholder={placeholder}
+  value={value}
+  onInputChange={handleInputChange}
+  onRemove={handleRemove}
+  onClear={handleClear}
+/>
+```
+
+## Contributing
+
+Contributions are what make the open-source community such an amazing place to learn, inspire, and
+create. All contributions are greatly appreciated.
+
+## License
+Distributed under the MIT License. See LICENSE for more information.
---
+++ packages/react/src/Taglicious.tsx
@@ -0,0 +1,113 @@
+import * as React from "react";
+
+import { InputChangeAction, Tag } from "@taglicious/model";
+
+export interface RenderInputProps {
+  placeholder?: string;
+  inputValue: string;
+  onChange: (ev: React.ChangeEvent<HTMLInputElement>) => void;
+  onKeyDown: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+}
+
+export interface RenderTagProps {
+  tag: Tag;
+  onRemove: (ev: React.MouseEvent<HTMLElement>, tag: Tag) => void;
+}
+
+interface Props {
+  placeholder?: string;
+  value: Tag[];
+
+  onInputChange(input: string, action: InputChangeAction): boolean | Promise<boolean>;
+  onRemove?(tag: Tag): void;
+  onClear?(): void;
+
+  renderInput(props: RenderInputProps): React.ReactNode;
+  renderTag(props: RenderTagProps): React.ReactNode;
+  renderClearButton(props: { clear: () => void }): React.ReactNode;
+}
+
+export function Taglicious({
+  placeholder,
+  value,
+  onInputChange,
+  onRemove,
+  onClear,
+  renderInput,
+  renderTag,
+  renderClearButton,
+}: Props) {
+  const isMountedRef = React.useRef(true);
+  const [inputValue, setInputValue] = React.useState("");
+
+  React.useEffect(() => {
+    isMountedRef.current = true;
+
+    return () => {
+      isMountedRef.current = false;
+    };
+  }, []);
+
+  function clear() {
+    onInputChange("", InputChangeAction.filter);
+    onClear?.();
+    setInputValue("");
+  }
+
+  const handleKeyDown = React.useCallback(
+    async (event: React.KeyboardEvent<HTMLInputElement>) => {
+      switch (event.key) {
+        case "Enter": {
+          const r = await onInputChange(inputValue.trim(), InputChangeAction.add);
+          if (isMountedRef.current && r === true) setInputValue("");
+          return;
+        }
+        case "Escape":
+          clear();
+          return;
+      }
+    },
+    [inputValue, onInputChange],
+  );
+
+  const handleChange = React.useCallback(
+    (ev: React.ChangeEvent<HTMLInputElement>) => {
+      const next = ev.target.value;
+      setInputValue(next);
+      onInputChange(next.trim(), InputChangeAction.filter);
+    },
+    [onInputChange],
+  );
+
+  const handleRemove = React.useCallback(
+    (ev: React.MouseEvent<HTMLElement>, tag: Tag) => {
+      ev.stopPropagation();
+      onRemove?.(tag);
+    },
+    [onRemove],
+  );
+
+  return (
+    <div className="taglicious form-select">
+      <div className="outer-container">
+        <div className="input-container">
+          {[...value].map((tag, index) => (
+            <React.Fragment key={index}>
+              {renderTag({ tag, onRemove: handleRemove })}
+            </React.Fragment>
+          ))}
+          {renderInput({
+            inputValue,
+            onChange: handleChange,
+            onKeyDown: handleKeyDown,
+            placeholder,
+          })}
+        </div>
+
+        <div className="controls">
+          {(value.length > 0 || inputValue) && renderClearButton({ clear })}
+        </div>
+      </div>
+    </div>
+  );
+}
