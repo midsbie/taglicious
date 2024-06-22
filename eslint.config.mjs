1ce34e3434edab55b0f5e267d1e805221f11b31b
Miguel Guedes <miguel@softgeist.com>
2024-06-22 14:24:47 +0100

    eslint: ignore dist directory

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,15 +2,22 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
-  rules: {
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-        caughtErrorsIgnorePattern: "^_",
-      },
-    ],
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
   },
-});
+  {
+    ignores: ["**/dist/"],
+  },
+);
