2f0443e4b32b5af304572783d475cc0e902b04fa
Miguel Guedes <miguel@softgeist.com>
2024-07-01 18:18:22 +0100

    lint: fix eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config({
-  files: ["** /*.ts"],
+  files: ["**/*.ts", "**/*.tsx"],
   extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
